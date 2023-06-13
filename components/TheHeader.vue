ff590e65d74f591e3d912df45b557023c885d2d6
specialone33 <ariser6@hotmail.com>
2023-06-13 16:44:56 +0300

    Initial commit

---
+++ components/TheHeader.vue
@@ -0,0 +1,3 @@
+<template>
+ HEADER
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  pages: true,
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"'
+        }
+      ]
+    }
+  }
+})
